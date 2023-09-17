c9158860693fc3b57e43eca04321b8b8c3ab5a50
Maciej Górecki <maciekgorecki@icloud.com>
2023-09-17 12:35:06 +0200

    Cleanup.

--- Sources/App/Controllers/ProductController.swift
+++ Sources/App/Controllers/ProductController.swift
@@ -4,8 +4,11 @@ import FluentKit
 import Foundation
 import Vapor
 
-// TODO: Cleanup all of this
 struct ProductController: RouteCollection {
+  enum Error: Swift.Error {
+    case buildProduct
+  }
+
   func boot(routes: Vapor.RoutesBuilder) throws {
     routes.get("products") { request async throws -> [Product] in
       do {
@@ -22,13 +25,7 @@ struct ProductController: RouteCollection {
     }
 
     routes.put("products", ":id", "like") { req in
-      let id = req.parameters.get("id")!
-      let uuid = UUID(uuidString: id)!
-
-      let product = try await DBProduct.query(on: req.db)
-        .filter(\.$id == uuid)
-        .first()!
-
+      let product = try await req.queryProduct(idParam: "id")
       product.isLiked = true
 
       try await product.update(on: req.db)
@@ -36,21 +33,14 @@ struct ProductController: RouteCollection {
       let imageURL = try req.imageURL(named: product.imageName)
 
       guard let product = Product(product, imageURL: imageURL) else {
-        struct CantCreateProductError: Error {}
-        throw CantCreateProductError()
+        throw Error.buildProduct
       }
 
       return product
     }
 
     routes.put("products", ":id", "dislike") { req in
-      let id = req.parameters.get("id")!
-      let uuid = UUID(uuidString: id)!
-
-      let product = try await DBProduct.query(on: req.db)
-        .filter(\.$id == uuid)
-        .first()!
-
+      let product = try await req.queryProduct(idParam: "id")
       product.isLiked = false
 
       try await product.update(on: req.db)
@@ -58,8 +48,7 @@ struct ProductController: RouteCollection {
       let imageURL = try req.imageURL(named: product.imageName)
 
       guard let product = Product(product, imageURL: imageURL) else {
-        struct CantCreateProductError: Error {}
-        throw CantCreateProductError()
+        throw Error.buildProduct
       }
 
       return product
@@ -68,6 +57,13 @@ struct ProductController: RouteCollection {
 }
 
 private extension Request {
+  enum Error: Swift.Error {
+    case invalidURL
+    case missingID
+    case initUUID
+    case noProduct
+  }
+
   func imageURL(named: String) throws -> URL {
     let hostname = application.http.server.configuration.hostname
     let port = application.http.server.configuration.port
@@ -76,10 +72,29 @@ private extension Request {
     let imageURL = baseURL + imagePath + named
 
     guard let url = URL(string: imageURL) else {
-      struct InvalidURLError: Error {}
-      throw InvalidURLError()
+      throw Error.invalidURL
     }
 
     return url
+  }
+
+  func queryProduct(idParam: String) async throws -> DBProduct {
+    guard let id = parameters.get(idParam) else {
+      throw Error.missingID
+    }
+
+    guard let uuid = UUID(uuidString: id) else {
+      throw Error.initUUID
+    }
+
+    let product = try await DBProduct.query(on: db)
+      .filter(\.$id == uuid)
+      .first()
+
+    guard let product else {
+      throw Error.noProduct
+    }
+
+    return product
   }
 }
