3241f88b60f341cccdf1f597254f8ae2656b4094
Maciej Górecki <maciekgorecki@icloud.com>
2023-09-23 13:48:56 +0200

    Add base cart models.

--- Sources/App/Controllers/CartController.swift
+++ Sources/App/Controllers/CartController.swift
@@ -5,7 +5,17 @@ import Foundation
 import Vapor
 
 struct CartController: RouteCollection {
+  enum Error: Swift.Error {
+    case userIdNotFound
+  }
+
   func boot(routes: RoutesBuilder) throws {
-    
+    routes.get("cart") { req in
+      guard let user = req.headers.first(name: "user") else {
+        throw Error.userIdNotFound
+      }
+
+      return HTTPStatus.ok
+    }
   }
 }
