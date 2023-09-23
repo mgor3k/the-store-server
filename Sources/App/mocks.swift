16ef1384231374dfdcd7fd412949f38aca4ce6cf
Maciej Górecki <maciekgorecki@icloud.com>
2023-09-23 13:12:39 +0200

    Added more mocks.

--- Sources/App/mocks.swift
+++ Sources/App/mocks.swift
@@ -19,12 +19,22 @@ func mocks(_ app: Application) async throws {
   let product2 = DBProduct(
     name: "Nike Air Pegasus",
     price: 250.40,
-    hexColor: "#FFD9D8",
+    hexColor: "#DBE3E6",
     imageName: "nike2.png",
     availableSizes: [34, 35, 36, 37],
     isLiked: false
   )
 
+  let product3 = DBProduct(
+    name: "Nike Air Max",
+    price: 99.29,
+    hexColor: "FFD9DD",
+    imageName: "nike3.png",
+    availableSizes: [22, 38, 40, 41, 42, 43, 44],
+    isLiked: false
+  )
+
   try await product1.create(on: app.db)
   try await product2.create(on: app.db)
+  try await product3.create(on: app.db)
 }
